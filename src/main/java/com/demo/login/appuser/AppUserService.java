43074e1797a0f5fe76a43af8412935dec3ce736a
Soojung Kwak <gsj2464@gmail.com>
2021-06-12 09:54:57 +0900

    Registration 기능 구현

--- src/main/java/com/demo/login/appuser/AppUserService.java
+++ src/main/java/com/demo/login/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,28 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository repository;
+    private final BCryptPasswordEncoder encoder;
     private final static String USER_NOT_FOUND = "user with email %s not found";
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = repository.findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if(userExists) {
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = encoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+
+        repository.save(appUser);
+
+        return "it works";
     }
 }
--- src/main/java/com/demo/login/registration/RegistrationService.java
+++ src/main/java/com/demo/login/registration/RegistrationService.java
@@ -1,10 +1,32 @@
 package com.demo.login.registration;
 
+import com.demo.login.appuser.AppUser;
+import com.demo.login.appuser.AppUserRole;
+import com.demo.login.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "it works!!!!";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
